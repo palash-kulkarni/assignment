40e27c165d8f7fc462f6a54d5445f53f009d7c60
palash-webonise <palash.kulkarni@weboniselab.com>
2014-07-28 10:55:53 +0530

    Delete task7.sh

--- task7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir public_html/public_html
-for file in *.*
-do
-	if [ ! -d $file ]; then
-		mv $file public_html/public_html;
-	fi
-done
-mv public_html/public_html public_html/public_html1
